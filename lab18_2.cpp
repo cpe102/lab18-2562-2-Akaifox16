bbb3a9a65c0f651ba643f7eb4866925e79a92327
Akaifox16 <kamonpat_s@cmu.ac.th>
2020-03-10 17:58:11 +0700

    V.1

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,1 +1,39 @@
 //Modify function overlap() form lab18_1.cpp by using pointers as input arguments.
+#include<iostream>
+using namespace std;
+
+struct Rect{
+	double x,y,w,h;
+};
+
+double overlap(Rect *,Rect *);
+
+int main(){
+	float x , y , w ,h;
+	cout << "Please input Rect 1 (x y w h): ";
+	cin >> x >> y >> w >> h ;
+	Rect A ={x, y, w ,h};
+	cout << "Please input Rect 2 (x y w h): ";
+	cin >> x >> y >> w >> h ;
+	Rect B = {x,y,w ,h};
+	cout << "Overlap area = " << overlap(&A,&B);	
+	return 0;
+}
+
+double overlap(Rect *a , Rect *b){
+	Rect ovl;
+	double fin , min ,max, ra, rb;
+	ra = a->x + a->w;
+	rb =  b->x + b->w;
+	min = ra > rb ? rb : ra; 
+	max = a->x > b->x ? a->x : b->x;
+	ovl.w = min - max ;
+
+	ra = a->y - a->h; 
+	rb = b->y - b->h;
+	min = a->y > b->y ? b->y : a->y; 
+	max = ra > rb ? ra : rb;
+	ovl.h = min - max;
+	double area = ovl.w * ovl.h;
+	return area > 0 ? area : 0;
+}
--- lab18_3.cpp
+++ lab18_3.cpp
@@ -9,6 +9,10 @@ using namespace std;
 
 struct student{
 	//Define struct student with four member (name ,id , gender, gpa);
+	string name ;
+	int id;
+	char gender;
+	float gpa;
 };
 
 struct course{
@@ -18,7 +22,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //Correct this line
+student * findstudent(vector<student>  &allstudents,int key){ //Correct this line
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -56,12 +60,16 @@ int main(){
 	
 	while(getline(student_file,textline)){
 		student s; 
-	
 		//Assign value to the members of struct s;
-	
+		char ss[50] , gen;
+		int id ; 
+		float gpa;
+		sscanf(textline.c_str(),"%[^,],%d,%c,%f",ss,&id,&gen,&gpa);
+		s = {ss, id , gen ,gpa}; 
 		allstudents.push_back(s); 		
 	}
 	
+
 	int state = 1;
 	while(getline(course_file,textline)){
 		if(state == 1){
@@ -77,6 +85,7 @@ int main(){
 				state = 3;
 			}else{
 				//Append lecture_list;
+				allcourses[allcourses.size() -1].lecture_list.push_back(textline);
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
@@ -84,6 +93,7 @@ int main(){
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
 				//Append student_list;
+				allcourses[allcourses.size() - 1].student_list.push_back(p);
 			}
 		}
 	}
